1a443589a587e1f1769ee2d08e5b8f53e1bc01dc
TiFcode <35370681+TiFcode@users.noreply.github.com>
2025-12-15 22:18:35 +0200

    Initial Commit

---
+++ TiFpuzzle2/TiFpuzzle2App.swift
@@ -0,0 +1,17 @@
+//
+//  TiFpuzzle2App.swift
+//  TiFpuzzle2
+//
+//  Created by Florin on 12/15/25.
+//
+
+import SwiftUI
+
+@main
+struct TiFpuzzle2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TiFpuzzle2Tests/TiFpuzzle2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  TiFpuzzle2Tests.swift
+//  TiFpuzzle2Tests
+//
+//  Created by Florin on 12/15/25.
+//
+
+import Testing
+@testable import TiFpuzzle2
+
+struct TiFpuzzle2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
